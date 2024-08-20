305f0b6621c626a10ff381e6ee79ba92e1b1005a
bilaljanati <bilaljanati830@gmail.com>
2024-08-20 13:23:27 +0200

    description

--- perceptron.py
+++ perceptron.py
@@ -1,1 +1,24 @@
 import numpy as np
+"""Perceptron classifier
+
+Parameters:
+------------
+eta: float
+    Learning rate (between 0 and 1)
+n_iter: int
+    Number of passes over the training dataset.
+random_state: int
+    Random number for weight initialization
+
+Attributes:
+------------
+w_ : 1d-array
+    Weights after fitting
+b_ : Scalar
+    Bias unit after fitting (threshold)
+
+errors_ : list
+    Number of misclassifications (updates) in each epoch.
+
+"""
+
