30abb1d72515ef03442867eb38735dc2599e6a53
bilaljanati <bilaljanati830@gmail.com>
2024-08-20 17:44:53 +0200

    corrected indentation errors

--- perceptron.py
+++ perceptron.py
@@ -1,24 +1,67 @@
 import numpy as np
-"""Perceptron classifier
+class Perceptron:
+    """Perceptron classifier
 
-Parameters:
-------------
-eta: float
-    Learning rate (between 0 and 1)
-n_iter: int
-    Number of passes over the training dataset.
-random_state: int
-    Random number for weight initialization
+    Parameters:
+    ------------
+    eta: float
+        Learning rate (between 0 and 1)
+    n_iter: int
+        Number of passes over the training dataset.
+    random_state: int
+        RNG seed for weight initialization
 
-Attributes:
-------------
-w_ : 1d-array
-    Weights after fitting
-b_ : Scalar
-    Bias unit after fitting (threshold)
+    Attributes:
+    ------------
+    w_ : 1d-array
+        Weights after fitting
+    b_ : Scalar
+        Bias unit after fitting (threshold)
 
-errors_ : list
-    Number of misclassifications (updates) in each epoch.
+    errors_ : list
+        Number of misclassifications (updates) in each epoch.
 
-"""
+    """
+#use the init function to define the properties of an object with the Perceptron class
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
 
+    def fit(self, X, y):
+        """Fit training data.
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+            Training vectors, where n_examples is the number of
+            examples and n_features is the number of features.
+        y : array-like, shape = [n_examples]
+            Target values.
+        Returns
+        -------
+        self : object
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi)) #creates the update value ∆w for each weight by comparing the predicted value to the actual label
+                self.w_ += update * xi
+                self.b_ += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+
+                                                                
